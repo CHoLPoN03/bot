7dc2998c80acb6f345ba74caa2a3d2ec73508712
CHoLPoN03 <cholponj6@gmail.com>
2024-06-18 13:35:35 +0600

    first commit

--- handlers/review.py
+++ handlers/review.py
@@ -4,6 +4,8 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from datetime import datetime
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+from config import database
+
 
 review_router = Router()
 
@@ -16,10 +18,12 @@ class BookSurvey(StatesGroup):
     cleanliness = State()  # чистота
     comment = State()  # комментарий
 
+
 @review_router.message(Command("review"))
 async def start_review(message: types.Message, state: FSMContext):
     await state.set_state(BookSurvey.name)  # устанавливаем состояние
     await message.answer("Как Вас зовут?")
+
 
 @review_router.message(Command("stop"))
 @review_router.message(F.text == "стоп")
@@ -27,17 +31,20 @@ async def stop_opros(message: types.Message, state: FSMContext):
     await state.clear()
     await message.answer("Спасибо! Ваш опрос закончен.")
 
+
 @review_router.message(BookSurvey.name)
 async def process_name(message: types.Message, state: FSMContext):
     await state.update_data(name=message.text)
     await state.set_state(BookSurvey.number)
     await message.answer("Ваш номер телефона или оставьте ваш инстаграм")
 
+
 @review_router.message(BookSurvey.number)
 async def process_number(message: types.Message, state: FSMContext):
     await state.update_data(number=message.text)
     await state.set_state(BookSurvey.date)
     await message.answer("Дата вашего посещения нашего заведения (в формате ДД.ММ.ГГГГ)")
+
 
 @review_router.message(BookSurvey.date)
 async def process_date(message: types.Message, state: FSMContext):
@@ -62,6 +69,7 @@ async def process_date(message: types.Message, state: FSMContext):
         print("Ошибка:", e)
         await message.answer("Пожалуйста, введите дату в формате ДД.ММ.ГГГГ")
 
+
 @review_router.message(BookSurvey.food)
 async def process_food(message: types.Message, state: FSMContext):
     if message.text not in ["1", "2", "3", "4", "5"]:
@@ -78,6 +86,7 @@ async def process_food(message: types.Message, state: FSMContext):
     await state.set_state(BookSurvey.cleanliness)
     await message.answer("Как оцениваете чистоту заведения?", reply_markup=keyboard)
 
+
 @review_router.message(BookSurvey.cleanliness)
 async def process_cleanliness(message: types.Message, state: FSMContext):
     if message.text not in ["1", "2", "3", "4", "5"]:
@@ -88,10 +97,18 @@ async def process_cleanliness(message: types.Message, state: FSMContext):
     await state.set_state(BookSurvey.comment)
     await message.answer("Ваши дополнительные комментарии", reply_markup=ReplyKeyboardRemove())
 
+
 @review_router.message(BookSurvey.comment)
 async def process_comment(message: types.Message, state: FSMContext):
     await state.update_data(comment=message.text)
     data = await state.get_data()
+
+    # сохранение data в БД
+    await database.execute("""
+            INSERT INTO review_results (name, number, date, food, cleanliness, comment)
+            VALUES (?, ?, ?, ?, ?, ?)""",
+                           (data['name'], data['number'], data['date'], data['food'], data['cleanliness'], data['comment'])
+                           )
     await state.clear()
     await message.answer(f"Спасибо за отзыв!\n\n"
                          f"Имя: {data.get('name')}\n"
@@ -101,7 +118,3 @@ async def process_comment(message: types.Message, state: FSMContext):
                          f"Чистота заведения: {data.get('cleanliness')}\n"
                          f"Комментарии: {data.get('comment')}")
     await message.answer("Спасибо за отзыв!")
-
-
-
-
